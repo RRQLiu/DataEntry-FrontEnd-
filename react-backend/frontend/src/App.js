bf474d2bf0f855ff090aeedf3994caccff9a1445
Ruiqi Liu <ruiqiliu@bu.edu>
2023-04-20 17:17:14 -0400

    init

---
+++ react-backend/frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import axios from "axios";
+
+const API_URL = "https://appcontainer.gentleglacier-98bdc4b1.eastus.azurecontainerapps.io/api/HttpTrigger1";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(API_URL, { data: inputValue });
+      console.log(response.data);
+      setInputValue("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <Typography variant="h4" sx={{ mb: 2 }}>
+        Put your data entry
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <TextField
+          label="Enter your data here"
+          variant="outlined"
+          size="large"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+          sx={{ width: "50vw" }}
+        />
+        <Button variant="contained" onClick={handleSubmit} sx={{ mt: 2, width: "50vw", maxWidth: "200px" }}>
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default App;
